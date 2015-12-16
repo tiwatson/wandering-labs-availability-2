8513b898bdedcbcedfd672201679a6bf240d5f26
Tim Watson <tiwatson@gmail.com>
2015-12-15 17:35:31 -0700

    ESLint Part 6

--- worker/src/app.js
+++ worker/src/app.js
@@ -1,26 +1,25 @@
 import _ from 'lodash';
 import Promise from 'bluebird';
 
-import { config } from './shared/utils/config';
+import { config } from './shared/utils/config'; //eslint-disable-line
 import { AvailabilityRequestRepo } from './shared/repos/availability-request';
 import { Sns } from './shared/utils/sns';
 
-exports.handler = function(event,context) {
+exports.handler = (event, context) => {
   return new AvailabilityRequestRepo().active().then((availabilityRequests) => {
-    let ids = _.map(availabilityRequests, (availabilityRequest) => {
+    const ids = _.map(availabilityRequests, (availabilityRequest) => {
       return availabilityRequest.id;
     });
     if (ids.length > 0) {
       return Promise.map(_.chunk(ids, 10), (chunkIds) => {
-        let idsString = _.shuffle(chunkIds).join(',');
-        return new Sns('scraper').publish(idsString).then(()=> {
+        const idsString = _.shuffle(chunkIds).join(',');
+        return new Sns('scraper').publish(idsString).then(() => {
           console.log('Sent SNS for:', idsString);
         });
-      }).then(()=> {
+      }).then(() => {
         context.succeed('sent active requests');
       });
-    }
-    else {
+    } else {
       context.succeed('No active requests');
     }
   });
