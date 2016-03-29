2dd0c814c992853233c075a8c0ad19bfb616b548
Tim Watson <tiwatson@gmail.com>
2016-03-28 20:11:51 -0600

    Toggle premium ask

--- notify/test/notifications/welcome.test.js
+++ notify/test/notifications/welcome.test.js
@@ -8,38 +8,70 @@ import { NotificationsWelcome } from '../../src/notifications/welcome';
 import { AvailabilityRequest, AvailabilityRequestRepo } from '../../src/shared/repos/availability-request'
 
 describe('NotificationsWelcome', ()=> {
-  let notificationsWelcome;
-  let availabilityRequest;
 
-  before(() => {
-    availabilityRequest = new AvailabilityRequest( ModelData.availabilityRequest({id: '1234-asdf'}) );
-    notificationsWelcome = new NotificationsWelcome(availabilityRequest);
-
-    Nocks.sendgrid();
-  });
-
-  describe('#compileTemplate', ()=> {
-    let html;
+  describe('premium member', ()=> {
+    let notificationsWelcome;
+    let availabilityRequest;
 
     before(() => {
-      html = notificationsWelcome._compileTemplate();
-      fs.writeFile('welcome.html', html);
+      availabilityRequest = new AvailabilityRequest( ModelData.availabilityRequest({id: '1234-asdf', premium: true}) );
+      notificationsWelcome = new NotificationsWelcome(availabilityRequest);
     });
 
-    it('compiles the template', ()=> {
-      expect(html).to.include(availabilityRequest.typeSpecific.parkName);
+    describe('#compileTemplate', ()=> {
+      let html;
+
+      before(() => {
+        html = notificationsWelcome._compileTemplate();
+        //fs.writeFile('welcome.html', html);
+      });
+
+      it('compiles the template', ()=> {
+        expect(html).to.include(availabilityRequest.typeSpecific.parkName);
+      });
+
+      it('includes the view link', ()=> {
+        expect(html).to.include('http://reserve.wanderinglabs.com/' + availabilityRequest.id + '/cancel');
+      });
+
+      it('adjusts content if you are a premium member', ()=> {
+        expect(html).to.include('You are a supporter')
+      });
+
     });
 
-    it('includes the view link', ()=> {
-      expect(html).to.include('http://reserve.wanderinglabs.com/' + availabilityRequest.id + '/cancel');
-    })
+    describe('#deliver', ()=> {
+      before(() => {
+        Nocks.sendgrid();
+      });
 
+      it('delivers the email', ()=> {
+        return notificationsWelcome.deliver().then((response)=> {
+          expect(response.message).to.equal('success');
+        });
+      });
+    });
   });
 
-  describe('#deliver', ()=> {
-    it('delivers the email', ()=> {
-      return notificationsWelcome.deliver().then((response)=> {
-        expect(response.message).to.equal('success');
+  describe('non premium member', ()=> {
+    let notificationsWelcome;
+    let availabilityRequest;
+
+    before(() => {
+      availabilityRequest = new AvailabilityRequest( ModelData.availabilityRequest({id: '1234-asdf'}) );
+      notificationsWelcome = new NotificationsWelcome(availabilityRequest);
+    });
+
+    describe('#compileTemplate', ()=> {
+      let html;
+
+      before(() => {
+        html = notificationsWelcome._compileTemplate();
+        //fs.writeFile('welcome.html', html);
+      });
+
+      it('asks you to support', ()=> {
+        expect(html).to.include('Help out and become a Premium Member')
       });
     });
   });
